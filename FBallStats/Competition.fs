5fa4999c23b16cc8caee6b125ec9e60185b7c74b
Niels Kloster Andreassen <nika@telenor.dk>
2013-07-14 22:55:09 +0200

    Initial check for unique name

---
+++ FBallStats/Competition.fs
@@ -0,0 +1,20 @@
+﻿module Competition
+
+type State = {
+    players : string list
+}
+with static member Zero = { players = [] }
+
+type Command =
+    | Add of string
+
+type Event =
+    | PlayerAdded of string
+
+module private Assert =
+    let contains name list = List.exists (fun elem -> elem = name) list
+    let thatPlayerIsNew state name = if contains name state.players then invalidArg "name" "Player is already in competition" else (state, name)
+
+let apply state = function
+    | PlayerAdded name -> Assert.thatPlayerIsNew state name |> fun (state, name) -> { state with players = name :: state.players }
+
